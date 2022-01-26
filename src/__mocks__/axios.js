e53f034ad9126bfdc4966326492a44a46d6796a1
Adaobiokeke <adaobiotiji@yahoo.com>
2022-01-25 19:37:02 -0500

    updated axios.js with put method

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -52,6 +52,11 @@ const fixtures = {
       }
     }
   };
+  const interview ={ 
+    student: "Lydia Miller-Jones",
+    interviewer: "Sylvia Palmer"
+  }
+  
   export default {
     get: jest.fn(url => {
       if (url === "/api/days") {
@@ -79,5 +84,13 @@ const fixtures = {
             data: fixtures.interviewers
           });
       }
-    })
+    }),
+      put: jest.fn(() => {
+        return Promise.resolve({
+          status: 200,
+          statusText: "OK",
+          data: interview
+        })
+      }),
+    
   }
